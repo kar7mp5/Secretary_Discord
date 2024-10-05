15f741c29f0aa2b5bfa59d0f5df45895e1dbd449
kar7mp5 <tommy1005a@gmail.com>
2024-10-06 04:29:49 +0900

    ES mode

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
+// index.js
+import { Client, GatewayIntentBits, ActivityType } from 'discord.js';
+import dotenv from 'dotenv';
 
-const client = new Discord.Client();
-client.login(config.BOT_TOKEN);
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+// setup message
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("정상화 중...", {
+        type: ActivityType.Listening,
+    });
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
